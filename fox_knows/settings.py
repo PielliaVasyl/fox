5c01991bb0ba8b6d3fd739b864d6c46d715ebf79
Vasyl Piellia <pellia@ukr.net>
2017-06-05 15:17:54 +0300

    Settings and basic templates

--- fox_knows/settings.py
+++ fox_knows/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0g2z3xm+iuhjkr&$7&civmj06)t5p+(%!v7%@oo85=0$$unu_z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3d party libraries
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'fox_knows.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates', 'atropos')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,  'atropos'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
