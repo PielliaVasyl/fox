678a043d08bfbe30b5d7b35d9bb4786d31d56628
Vasyl Piellia <pellia@ukr.net>
2017-06-06 14:21:47 +0300

    url dispatcher with city- and direction- param

--- fox_knows/urls.py
+++ fox_knows/urls.py
@@ -13,17 +13,58 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from home_page import views as home_page_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home_page_views.index),
-    url(r'^feed/', home_page_views.index),
-    url(r'^map/', home_page_views.index),
-    url(r'^events/', home_page_views.index),
-    url(r'^ratings/', home_page_views.index),
+
+    url(r'^', include([
+        url(r'^$', home_page_views.index),
+        url(r'^(?:city-(?P<city_title>[\w-]+)/)?', include([
+            url(r'^$', home_page_views.index),
+            url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+        ])),
+        url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+
+    ])),
+    url(r'^feed/', include([
+        url(r'^$', home_page_views.index),
+        url(r'^(?:city-(?P<city_title>[\w-]+)/)?', include([
+            url(r'^$', home_page_views.index),
+            url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+        ])),
+        url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+
+    ])),
+    url(r'^map/', include([
+        url(r'^$', home_page_views.index),
+        url(r'^(?:city-(?P<city_title>[\w-]+)/)?', include([
+            url(r'^$', home_page_views.index),
+            url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+        ])),
+        url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+
+    ])),
+    url(r'^events/', include([
+        url(r'^$', home_page_views.index),
+        url(r'^(?:city-(?P<city_title>[\w-]+)/)?', include([
+            url(r'^$', home_page_views.index),
+            url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+        ])),
+        url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+
+    ])),
+    url(r'^ratings/', include([
+        url(r'^$', home_page_views.index),
+        url(r'^(?:city-(?P<city_title>[\w-]+)/)?', include([
+            url(r'^$', home_page_views.index),
+            url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+        ])),
+        url(r'^(?:direction-(?P<direction_title>[\w-]+)/)?$', home_page_views.index),
+
+    ])),
 
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 
-def index(request):
+def index(request, city_title=None, direction_title=None):
     title = 'Сообщество активных людей'
     context = {
         'title': title,
+        'city_title': city_title,
+        'direction_title': direction_title
     }
     return render(request, 'base.html', context)
